5266270353f3ebc6de65f2e39409ac74459ca6d9
Hsu-Huahsing <huahsing412@gmail.com>
2022-04-05 22:39:43 +0800

    111

--- packet.py
+++ packet.py
@@ -15,174 +15,194 @@ import pandas as pd
 import numpy as np
 now = datetime.now().date()
 
-crawlerdic={
+title_dic = {
+    "每日收盤行情" : [ "價格指數(臺灣證券交易所)","價格指數(跨市場)","價格指數(臺灣指數公司)","報酬指數(臺灣證券交易所)","報酬指數(跨市場)","報酬指數(臺灣指數公司)","大盤統計資訊","漲跌證券數合計","每日收盤行情" ] ,
+    '信用交易統計' : [ "融資融券彙總" , "信用交易統計" ] ,
+    '市場成交資訊' : [ '市場成交資訊' ] ,
+    '三大法人買賣金額統計表' : [ '三大法人買賣金額統計表' ] ,
+    '三大法人買賣超日報' : [ "三大法人買賣超日報" ] ,
+    '個股日本益比、殖利率及股價淨值比' : ['個股日本益比、殖利率及股價淨值比'] ,
+    '信用額度總量管制餘額表' : ['信用額度總量管制餘額表'],
+    '當日沖銷交易標的及成交量值' : ["當日沖銷交易標的及成交量值" , "當日沖銷交易統計資訊"] ,
+    '每月當日沖銷交易標的及統計' : ['每月當日沖銷交易標的及統計'],
+    '外資及陸資投資持股統計': ["外資及陸資投資持股統計","外資投資持股統計"] ,
+    '發行量加權股價指數歷史資料' : ['發行量加權股價指數歷史資料']
+}
+
+data_dic = {
     "每日收盤行情":{
         "m":"market",
         "freq":"D",
         "dname":"date",
         "date_min":"2004-2-11",
-        "payload":{"response": "json",
-                   "date": "",
-                   "type": "ALL",
-                   "_": "1613296592078"},
         "url":r"https://www.twse.com.tw/exchangeReport/MI_INDEX?",
-        "title":["價格指數(臺灣證券交易所)","價格指數(跨市場)","價格指數(臺灣指數公司)","報酬指數(臺灣證券交易所)","報酬指數(跨市場)","報酬指數(臺灣指數公司)","大盤統計資訊","漲跌證券數合計","每日收盤行情"]
-        },
+        "payload":{
+            "response": "json",
+            "date": "",
+            "type": "ALL",
+            "_": "1613296592078"
+        } ,
+    },
     "信用交易統計":{
         "m":"market",
         "freq":"D",
         "dname":"date",
         "date_min":"2001-1-1",
-        "payload":{"response": "json",
-                   "date": "",
-                   "selectType": "ALL"},
         "url":r"https://www.twse.com.tw/exchangeReport/MI_MARGN?",
-        "title":["融資融券彙總","信用交易統計"]
-        },
+        "payload":{
+            "response": "json",
+            "date": "",
+            "selectType": "ALL"
+        } ,
+    } ,
     "市場成交資訊":{
         "m":"market",
         "freq":"M",
         "date_min":"1990-1-4",
         "dname":"date",
-        "payload":{"response": "json",
-                   "date": "",
-                   "_":"1613392395864"},
         "url":r"https://www.twse.com.tw/exchangeReport/FMTQIK?",
-        "title":["市場成交資訊"]
+        "payload":{
+            "response": "json",
+            "date": "",
+            "_":"1613392395864"
         },
+    },
     "三大法人買賣金額統計表":{
         "m":"market",
         "freq":"D",
         "date_min":"2004-4-7",
         "dname":"dayDate",
-        "payload":{"response": "json",
-                   "dayDate": "",
-                   "type": "day",
-                   "_":"1613389589646"},
         "url":r"https://www.twse.com.tw/fund/BFI82U?",
-        "title":["三大法人買賣金額統計表"]
+        "payload":{
+            "response": "json",
+            "dayDate": "",
+            "type": "day",
+            "_":"1613389589646"
         },
+    },
     "三大法人買賣超日報":{
         "m":"market",
         "freq":"D",
         "date_min":"2012-5-2",
         "dname":"date",
-        "payload":{"response": "json",
-                   "date": "",
-                   "selectType": "ALL"},
         "url":r"https://www.twse.com.tw/fund/T86?",
-        "title":["三大法人買賣超日報"]
-        },
+        "payload":{
+            "response": "json",
+            "date": "",
+            "selectType": "ALL"
+        } ,
+    } ,
     "個股日本益比、殖利率及股價淨值比":{
         "m":"stock",
         "freq":"D",
         "date_min":"2012-5-2",
         "dname":"date",
-        "payload":{"response": "json",
-                  "date": "",
-                  "selectType" : "ALL",
-                  "_": "1596117278906"},
         "url":r"https://www.twse.com.tw/exchangeReport/BWIBBU_d?",
-        "title": ["個股日本益比、殖利率及股價淨值比"]
+        "payload":{
+            "response": "json",
+            "date": "",
+            "selectType" : "ALL",
+            "_": "1596117278906"
         },
+    },
     "信用額度總量管制餘額表":{
         "m":"stock",
         "freq":"D",
         "date_min":"2005-7-1",
         "dname":"date",
-        "payload":{"response": "json",
-                   "date": "",
-                   "_": "1596721575815"},
         "url":r"https://www.twse.com.tw/exchangeReport/TWT93U?",
-        "title":["信用額度總量管制餘額表"]
-        },
+        "payload":{
+            "response": "json",
+            "date": "",
+            "_": "1596721575815"
+        } ,
+    } ,
     "當日沖銷交易標的及成交量值":{
         "m":"stock",
         "freq":"D",
         "date_min":"2014-1-6",
         "dname":"date",
-        "payload":{"response": "json",
-                   "date": "",
-                   "selectType": "All",
-                   "_": "1596117305431"},
         "url":r"https://www.twse.com.tw/exchangeReport/TWTB4U?",
-        "title":["當日沖銷交易標的及成交量值","當日沖銷交易統計資訊"]
+        "payload":{
+            "response": "json",
+            "date": "",
+            "selectType": "All",
+            "_": "1596117305431"
         },
+    },
     # 這裡的,"當日沖銷交易統計"跟market有重複，因為都是大盤的沖銷交易===========
     "每月當日沖銷交易標的及統計":{
         "m":"market",
         "freq":"M",
         "date_min":"2014-1-6",
         "dname":"date",
-        "payload":{"response": "json",
-                   "date": "",
-                   "stockNo": "",
-                   "_": "1596117360962"},
         "url":"https://www.twse.com.tw/exchangeReport/TWTB4U2?",
-        "title":["每月當日沖銷交易標的及統計"]
+        "payload":{
+            "response": "json",
+            "date": "",
+            "stockNo": "",
+            "_": "1596117360962"
         },
+    },
     "外資及陸資投資持股統計":{
         "m":"stock",
         "freq":"D",
         "date_min":"2004-2-11",
         "dname":"date",
-        "payload":{"response": "json",
-                   "date": "",
-                   "selectType": "ALLBUT0999",
-                   "_": "1594606204191"},
         "url":"https://www.twse.com.tw/fund/MI_QFIIS?",
-        "title":["外資及陸資投資持股統計","外資投資持股統計"]
+        "payload":{
+            "response": "json",
+            "date": "",
+            "selectType": "ALLBUT0999",
+            "_": "1594606204191"
         },
+    },
     "發行量加權股價指數歷史資料":{
         "m":"market",
         "freq":"D",
         "date_min":"1999-1-5",
         "dname":"date",
-        "payload":{"response": "json",
-                   "date": "",
-                   "_": "1597539490294"},
         "url":"https://www.twse.com.tw/indicesReport/MI_5MINS_HIST?",
-        "title": ["發行量加權股價指數歷史資料"]
+        "payload":{
+            "response": "json",
+            "date": "",
+            "_": "1597539490294"
         },
+    },
+    'stock' : {
+        "url":r"https://isin.twse.com.tw/isin/C_public.jsp?strMode={}",
+        "charset":"cp950"
+        
     }
-
-title_dic = {
-    t:_ for _ in crawlerdic for t in crawlerdic[_]["title"]
 }
 
-stocktable={"url":r"https://isin.twse.com.tw/isin/C_public.jsp?strMode={}",
-            "charset":"cp950"
-            }
+headers={
+    "safari14.0":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
+    "iphone13":"Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1",
+    "ipod13":"Mozilla/5.0 (iPod; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1",
+    "ipadmini13":"Mozilla/5.0 (iPad; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1",
+    "ipad":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Safari/605.1.15",
+    "winedge":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
+    "mac":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
+    "chromewin":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
+    "firefoxmac":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:70.0) Gecko/20100101 Firefox/70.0",
+    "firefoxwin":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0"
+}
 
-headers={"safari14.0":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
-         "iphone13":"Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1",
-         "ipod13":"Mozilla/5.0 (iPod; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1",
-         "ipadmini13":"Mozilla/5.0 (iPad; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1",
-         "ipad":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Safari/605.1.15",
-         "winedge":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
-         "mac":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
-         "chromewin":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
-         "firefoxmac":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:70.0) Gecko/20100101 Firefox/70.0",
-         "firefoxwin":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0"}
-
-def iter_headers(header=headers):
-    lis=[x for x in header.values()]
+def iter_headers( header = headers ) :
+    lis=[ x for x in header.values()]
     while True :
         r=random.randint(0,len(lis)-1)
         yield {"User-Agent":lis[r]}
 
-def datesplit(d, sp="-"):
-    d = d.split(sp)
-    return d[0] + d[1].zfill(2) + d[2].zfill(2)
-
-rename_dic={
+colname_dic={
     "上市認購(售)權證"                       :"上市認購售權證",
-    "臺灣存託憑證(TDR)"                    :"臺灣存託憑證",
+    "臺灣存託憑證(TDR)"                    :"台灣存託憑證",
     "受益證券-不動產投資信託"             :"受益證券_不動產投資信託",
     "國際證券辨識號碼(ISIN Code)"     :"ISINCode",
     "上櫃認購(售)權證"                       :"上櫃認購售權證",
     "受益證券-資產基礎證券"                :"受益證券_資產基礎證券",
-    "黃金期貨(USD)"                          :"黃金期貨",
+    "黃金期貨(USD)"                          :"黃金期貨USD",
     "成交金額(元)"                             :"成交金額_元",
     "成交股數(股)"                             :"成交股數_股",
     "漲跌百分比(%)"                           :"漲跌百分比%",
@@ -196,15 +216,14 @@ rename_dic={
     "外陸資買進股數(不含外資自營商)"   :"外陸資買進股數_不含外資自營商",
     "外陸資賣出股數(不含外資自營商)"   :"外陸資賣出股數_不含外資自營商",
     "外陸資買賣超股數(不含外資自營商)":"外陸資買賣超股數_不含外資自營商",
-    "現金(券)償還":"現金券償還",
-    "證券代號":"代號",
-    "證券名稱":"名稱",
-    "股票代號":"代號",
-    "指數代號":"代號",
-    "股票名稱":"名稱",
-    "有價證券名稱":"名稱",
-    
-    }
+    "現金(券)償還"                             : "現金券償還",
+    "證券代號"                                   :"代號",
+    "股票代號"                                   :"代號",
+    "指數代號"                                   :"代號",
+    "證券名稱"                                   :"名稱",
+    "股票名稱"                                   :"名稱",
+    "有價證券名稱"                             :"名稱",
+}
 
 def stocktablecrawl(maxn=13,timeout=180,pk="ISINCode"):
     dm = dbmanager(user="root")
